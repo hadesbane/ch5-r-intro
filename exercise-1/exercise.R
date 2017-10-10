e408a3e445d546ed706d7c31888b96bbb5c6c2d1
hadesbane <ryandbarth@gmail.com>
2017-10-09 17:58:18 -0700

    Done with the lab

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,18 +1,8 @@
-# Exercise-1: practice with basic syntax
-
-# Create a variable `hometown` that stores the city in which you were born
-
-# Assign your name to the variable `my.name`
-
-# Assign your height to a variable `my.height`
-
-# Create a variable `puppies` equal to the number of puppies you'd like to have
-
-# Create a variable `puppy.price`, which is how expensive you think a puppy is
-
-# Create a variable `total.cost` that has the total cost of all of your puppies
-
-# Create a boolean variable `too.expensive`, set to true if the cost is greater than $1,000
-
-# Create a variable `max_puppies`, which is the number of puppies you can afford for $1K.  Compute this
-# value in R, not just assign!
+hometown <- "Edmonds"
+myName <- "Ryan Barth"
+myHeight <- ("5'7\"")
+puppies <- 3
+puppyPrice <- 700
+totalCost <- puppies * puppyPrice
+tooExpensive <- totalCost > 1000
+max_puppies <- round(1000/puppyPrice)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,32 +1,19 @@
-## exercise 2 -- if/else statement
-## now you know how to do the basic syntax in R, let's play with if/else statements
-## the game is called 'is it going to rain in Seattle?'
+chance_of_rain <- 0
+I_saw_raindrops <- TRUE
+my_shoes_are_wet <- FALSE
+my_ta_is_soaked <- TRUE
+I_love_cookies <- TRUE
 
-## make a variable called 'chance_of_rain' and assign it to be 0
-
-## assign 'true' or 'false' to a variable named 'I_saw_raindrops'
-
-## assign 'true' or 'false' to a variable named 'my_shoes_are_wet'
-
-## assign 'true' or 'false' to a variable named 'my_ta_is_soaked'
-
-## assign 'true' or 'false' to a variable named 'I_love_cookies'
-
-## ok, now you have your variables ready to go.
-
-## make an if statement that checks whether you saw raindrops
-## if you did, set chance_of_rain to 100
-
-## else make an if statement that checks whether your shoes are wet
-## if they are, chance of rain goes up by 40 percent
-
-## else make an if statement that checks whether your TA is soaked
-## if you did, chance of rain goes up by 50 percent
-
-## make an if statement that checks whether you love cookies
-## if you don't, give a reason why not
-
-## Finally print out a sentence that says 'The chance of rain in Seattle is (the number stored in chance_of_rain variable) percent'
-
-## bonus:
-## how to make write this exercise in a function??
+if(I_saw_raindrops){
+  chance_of_rain <- 100
+}
+if(my_shoes_are_wet){
+  chance_of_rain <- chance_of_rain + 40
+}
+if(my_ta_is_soaked){
+  chance_of_rain <- chance_of_rain + 50
+}
+if(!I_love_cookies){
+  print("They arn't tasty")
+}
+print(chance_of_rain)
